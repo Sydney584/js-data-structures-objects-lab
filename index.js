a6c9a7a7c7362a5d424e795cb48f197f53d3a0aa
sydney <sydneychase5841@gmail.com>
2021-04-06 14:35:42 -0400

    Updated Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+const driver = {
+    name: 'Sam'
+    
+    
+};
+
+function updateDriverWithKeyAndValue() {
+    return Object.assign({address: '11 Broadway'}, driver);
+    
+};
+function destructivelyUpdateDriverWithKeyAndValue() {
+    driver.address = '12 Broadway';
+    return driver;
+};
